741e0a285995154d5dc939680120d2d2938d4e6f
Weiyang <35239596+wyzhang24@users.noreply.github.com>
2019-04-10 03:00:37 -0400

    add video record and matrix generation function

--- run_manni_main.m
+++ run_manni_main.m
@@ -1,8 +1,26 @@
 %   Author: Maani Ghaffari Jadidi
 %   Date:   January 1, 2019
+clear all
 
+makeVideo = false;
 % Register Two Point Clouds
-dataset_name = 'freiburg1_xyz';
+if makeVideo
+    try
+        votype = 'avifile';
+        vo = avifile('video.avi', 'fps', 5);
+    catch
+        votype = 'VideoWriter';
+        vo = VideoWriter('video', 'MPEG-4');
+        set(vo, 'FrameRate', 5);
+        open(vo);
+    end
+end
+
+
+
+
+%dataset_name = 'freiburg1_xyz';
+dataset_name = 'freiburg1_desk';
 % dataset_path = ...
 %     strcat('/path_to_your_dataset_folder/', ...
 %     dataset_name, '/');
@@ -10,15 +28,16 @@ dataset_name = 'freiburg1_xyz';
 dataset_path = ...
     strcat('rgbd_tum/', ...
     dataset_name, '/');
-pcd_full_path = strcat(dataset_path,'pcd_full')
+pcd_full_path = strcat(dataset_path,'pcd_full');
 %assoc = import_assoc_file(assoc_filename);
 
-
+transform_dict = {};
+transform_accumu_dict = {};
 pcd_full_dir_info = dir(fullfile(pcd_full_path, '*.pcd'));
 
-assoc_filename = strcat(dataset_path, 'assoc.txt');
-% Load rows startRow to endRow (or 1 to end if not chosen)
-assoc = import_assoc_file(assoc_filename,1,2);
+% assoc_filename = strcat(dataset_path, 'assoc.txt');
+% % Load rows startRow to endRow (or 1 to end if not chosen)
+% assoc = import_assoc_file(assoc_filename,1,2);
 
 % Make a cell of these filepath names
 num_pcd_files = length(pcd_full_dir_info);
@@ -55,7 +74,8 @@ rkhs_se3.align();
 toc
 fprintf('\n')
 tform = rkhs_se3.tform;
-
+transform_dict{1,1} = pcd_full_dir_info(1).name(1:end-4);
+transform_dict{1,2} = tform.T';
 % Transform the current point cloud to the reference coordinate system
 % defined by the first point cloud.
 % ptCloudAligned = pctransform(livingRoomData{2},tform);
@@ -79,7 +99,8 @@ ptCloudScene = pcmerge(ptCloudRef_full, ptCloudAligned, mergeSize);
 
 % Store the transformation object that accumulates the transformation.
 accumTform = tform; 
-
+transform_accumu_dict{1,1} = pcd_full_dir_info(1).name(1:end-4);
+transform_accumu_dict{1,2} = accumTform.T';
 figure
 hAxes = pcshow(ptCloudScene, 'VerticalAxis','Y', 'VerticalAxisDir', 'Down');
 title('Updated world scene')
@@ -87,49 +108,74 @@ title('Updated world scene')
 hAxes.CameraViewAngleMode = 'auto';
 hScatter = hAxes.Children;
 
-% for i = 3:length(livingRoomData)
-%     ptCloudCurrent = ptcloud_edge_filter(ptCouldCurrent_full);
-%        
-%     % Use previous moving point cloud as reference.
-%     fixed = moving;
-%     moving = pcdownsample(ptCloudCurrent, 'gridAverage', gridSize);
-%     
-%     % Apply RKHS registration.
-%     rkhs_se3.set_ptclouds(fixed, moving); 
-%     tic
-%     rkhs_se3.align();
-%     toc
-%     fprintf('\n')
-%     tform = rkhs_se3.tform;
-% 
-%     % Transform the current point cloud to the reference coordinate system
-%     % defined by the first point cloud.
-%     accumTform = affine3d(tform.T * accumTform.T);
-%     ptCloudAligned = pctransform(livingRoomData{i}, accumTform);
-%     
-%     % Update the world scene.
-%     ptCloudScene = pcmerge(ptCloudScene, ptCloudAligned, mergeSize);
-% 
-%     % Visualize the world scene.
-%     hScatter.XData = ptCloudScene.Location(:,1);
-%     hScatter.YData = ptCloudScene.Location(:,2);
-%     hScatter.ZData = ptCloudScene.Location(:,3);
-%     hScatter.CData = ptCloudScene.Color;
-%     drawnow('limitrate')
-% end
+for i = 3:iter
+    cur_frame_num = i
+    ptcloud_files{i,1};
+    ptCloudCurrent = ptcloud_edge_filter(pcread(ptcloud_files{i,1}));
+       
+    % Use previous moving point cloud as reference.
+    fixed = moving;
+    moving = pcdownsample(ptCloudCurrent, 'gridAverage', gridSize);
+    
+    % Apply RKHS registration.
+    rkhs_se3.set_ptclouds(fixed, moving); 
+    tic
+    rkhs_se3.align();
+    toc
+    fprintf('\n')
+    tform = rkhs_se3.tform;
+    
+    transform_dict{i-1,1} = pcd_full_dir_info(i-1).name(1:end-4);
+    transform_dict{i-1,2} = tform.T';
 
+    % Transform the current point cloud to the reference coordinate system
+    % defined by the first point cloud.
+    accumTform = affine3d((accumTform.T'*tform.T')');
+    ptCloudAligned = pctransform(pcread(ptcloud_files{i,1}), accumTform);
+    
+    transform_accumu_dict{i-1,1} = pcd_full_dir_info(i-1).name(1:end-4);
+
+    transform_accumu_dict{i-1,2} = accumTform.T';
+
+    % Update the world scene.
+    ptCloudScene = pcmerge(ptCloudScene, ptCloudAligned, mergeSize);
+
+    % Visualize the world scene.
+    hScatter.XData = ptCloudScene.Location(:,1);
+    hScatter.YData = ptCloudScene.Location(:,2);
+    hScatter.ZData = ptCloudScene.Location(:,3);
+    hScatter.CData = ptCloudScene.Color;
+    drawnow('limitrate')
+
+    if makeVideo
+        F = getframe(gcf);
+        switch votype
+          case 'avifile'
+            vo = addframe(vo, F);
+          case 'VideoWriter'
+            writeVideo(vo, F);
+          otherwise
+            error('unrecognized votype');
+        end
+    end
+    
+    
+end
+if makeVideo
+    close(vo)
+end
 % During the recording, the Kinect was pointing downward. To visualize the
 % result more easily, let's transform the data so that the ground plane is
 % parallel to the X-Z plane.
-angle = -pi/10;
-A = [1,0,0,0;...
-     0, cos(angle), sin(angle), 0; ...
-     0, -sin(angle), cos(angle), 0; ...
-     0 0 0 1];
-ptCloudScene = pctransform(ptCloudScene, affine3d(A));
-pcshow(ptCloudScene, 'VerticalAxis','Y', 'VerticalAxisDir', 'Down', ...
-        'Parent', hAxes)
-title('Updated world scene')
-xlabel('X (m)')
-ylabel('Y (m)')
-zlabel('Z (m)')
+% angle = -pi/10;
+% A = [1,0,0,0;...
+%      0, cos(angle), sin(angle), 0; ...
+%      0, -sin(angle), cos(angle), 0; ...
+%      0 0 0 1];
+% ptCloudScene = pctransform(ptCloudScene, affine3d(A));
+% pcshow(ptCloudScene, 'VerticalAxis','Y', 'VerticalAxisDir', 'Down', ...
+%         'Parent', hAxes)
+% title('Updated world scene')
+% xlabel('X (m)')
+% ylabel('Y (m)')
+% zlabel('Z (m)')
