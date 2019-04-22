4b81853f098f3e880f434d0174630a6f591242fc
Weiyang <35239596+wyzhang24@users.noreply.github.com>
2019-04-22 11:11:18 -0400

    The newest function fixing NaN bug

--- rgbd_tum/ptcloud_edge_filter.m
+++ rgbd_tum/ptcloud_edge_filter.m
@@ -1,19 +1,14 @@
-function clout_out = ptcloud_edge_filter(cloud_in)
+function clout_out = ptcloud_edge_filter_new(cloud_in)
 % extract edges from an organized RGB-D point cloud
+%   Author: Maani Ghaffari Jadidi
+%   Date:   January 1, 2019
 
-% BW is a 480x640 logical (i.e. true or false)
-BW = edge(rgb2gray(cloud_in.Color),'canny');
-% Reshape Location from 480x640x3 to 307200x3
+BW = edge(rgb2gray(im2double(cloud_in.Color)),'canny');
 X = reshape(cloud_in.Location,[], 3, 1);
-% Reshape Color from 480x640x3 to 307200x3
 C = reshape(cloud_in.Color,[], 3, 1);
-% Take only the values of X that were detected as edges by the Canny method
-X = X(reshape(BW, [], 1), :);
-% Take only the values of C that were detected as edges by the Canny method
-C = C(reshape(BW, [], 1), :);
-% Remove all rows of X that are NaN (Not a Number)
-X = X(~isnan(X(:,1)),:);
-% Remove all rows of C that are NaN (Not a Number)
+% X = X(reshape(BW, [], 1), :);
+% C = C(reshape(BW, [], 1), :);
 C = C(~isnan(X(:,1)),:);
+X = X(~isnan(X(:,1)),:);
 
 clout_out = pointCloud(X, 'Color', C);
