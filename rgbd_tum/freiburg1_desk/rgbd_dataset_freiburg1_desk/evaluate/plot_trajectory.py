ea277bf8ac0702d4c34b075ce97d9d74bb889227
ftliu1 <43532000+ftliu1@users.noreply.github.com>
2019-04-22 21:38:43 -0400

    Add files via upload
    plot trajectory

---
+++ rgbd_tum/freiburg1_desk/rgbd_dataset_freiburg1_desk/evaluate/plot_trajectory.py
@@ -0,0 +1,233 @@
+import sys
+import numpy
+import argparse
+import associate
+
+
+def align(model, data):
+    """Align two trajectories using the method of Horn (closed-form).
+
+    Input:
+    model -- first trajectory (3xn)
+    data -- second trajectory (3xn)
+
+    Output:
+    rot -- rotation matrix (3x3)
+    trans -- translation vector (3x1)
+    trans_error -- translational error per point (1xn)
+
+    """
+    numpy.set_printoptions(precision=3, suppress=True)
+    model_zerocentered = model - model.mean(1)
+    data_zerocentered = data - data.mean(1)
+
+    W = numpy.zeros((3, 3))
+    for column in range(model.shape[1]):
+        W += numpy.outer(model_zerocentered[:, column], data_zerocentered[:, column])
+    U, d, Vh = numpy.linalg.linalg.svd(W.transpose())
+    S = numpy.matrix(numpy.identity(3))
+    if (numpy.linalg.det(U) * numpy.linalg.det(Vh) < 0):
+        S[2, 2] = -1
+    rot = U * S * Vh
+    trans = data.mean(1) - rot * model.mean(1)
+
+    model_aligned = rot * model + trans
+    alignment_error = model_aligned - data
+
+    trans_error = numpy.sqrt(numpy.sum(numpy.multiply(alignment_error, alignment_error), 0)).A[0]
+
+    return rot, trans, trans_error
+
+
+def plot_traj(ax, stamps, traj, style, color, label):
+    """
+    Plot a trajectory using matplotlib.
+
+    Input:
+    ax -- the plot
+    stamps -- time stamps (1xn)
+    traj -- trajectory (3xn)
+    style -- line style
+    color -- line color
+    label -- plot legend
+
+    """
+    stamps.sort()
+    interval = numpy.median([s - t for s, t in zip(stamps[1:], stamps[:-1])])
+    x = []
+    y = []
+    last = stamps[0]
+    for i in range(len(stamps)):
+        if stamps[i] - last < 2 * interval:
+            x.append(traj[i][0])
+            y.append(traj[i][1])
+        elif len(x) > 0:
+            ax.plot(x, y, style, color=color, label=label)
+            label = ""
+            x = []
+            y = []
+        last = stamps[i]
+    if len(x) > 0:
+        ax.plot(x, y, style, color=color, label=label)
+
+
+if __name__ == "__main__":
+    # parse command line
+    parser = argparse.ArgumentParser(description='''
+    This script computes the absolute trajectory error from the ground truth trajectory and the estimated trajectory. 
+    ''')
+    parser.add_argument('first_file', help='ground truth trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('second_file', help='estimated trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('third_file', help='estimated trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('fourth_file', help='estimated trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('fifth_file', help='estimated trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('sixth_file', help='estimated trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('--offset', help='time offset added to the timestamps of the second file (default: 0.0)',
+                        default=0.0)
+    parser.add_argument('--scale', help='scaling factor for the second trajectory (default: 1.0)', default=1.0)
+    parser.add_argument('--max_difference',
+                        help='maximally allowed time difference for matching entries (default: 0.02)', default=0.02)
+    parser.add_argument('--save', help='save aligned second trajectory to disk (format: stamp2 x2 y2 z2)')
+    parser.add_argument('--save_associations',
+                        help='save associated first and aligned second trajectory to disk (format: stamp1 x1 y1 z1 stamp2 x2 y2 z2)')
+    parser.add_argument('--plot', help='plot the first and the aligned second trajectory to an image (format: png)')
+    parser.add_argument('--verbose',
+                        help='print all evaluation data (otherwise, only the RMSE absolute translational error in meters after alignment will be printed)',
+                        action='store_true')
+    args = parser.parse_args()
+
+    first_list = associate.read_file_list(args.first_file)
+    second_list = associate.read_file_list(args.second_file)
+    third_list = associate.read_file_list(args.third_file)
+    """fourth_list = associate.read_file_list(args.fourth_file)"""
+    fifth_list = associate.read_file_list(args.fifth_file)
+    sixth_list = associate.read_file_list(args.sixth_file)
+
+    matches = associate.associate(first_list, second_list, float(args.offset), float(args.max_difference))
+    if len(matches) < 2:
+        sys.exit(
+            "Couldn't find matching timestamp pairs between groundtruth and estimated trajectory! Did you choose the correct sequence?")
+
+    first_xyz = numpy.matrix([[float(value) for value in first_list[a][0:3]] for a, b in matches]).transpose()
+    second_xyz = numpy.matrix(
+        [[float(value) * float(args.scale) for value in second_list[b][0:3]] for a, b in matches]).transpose()
+    rot, trans, trans_error = align(second_xyz, first_xyz)
+
+    second_xyz_aligned = rot * second_xyz + trans
+
+    first_stamps = first_list.keys()
+    first_stamps.sort()
+    first_xyz_full = numpy.matrix([[float(value) for value in first_list[b][0:3]] for b in first_stamps]).transpose()
+
+    second_xyz = numpy.matrix(
+        [[float(value) * float(args.scale) for value in second_list[b][0:3]] for a, b in matches]).transpose()
+    rot, trans, trans_error = align(second_xyz, first_xyz)
+    second_stamps = second_list.keys()
+    second_stamps.sort()
+    second_xyz_full = numpy.matrix(
+        [[float(value) * float(args.scale) for value in second_list[b][0:3]] for b in second_stamps]).transpose()
+    second_xyz_full_aligned = rot * second_xyz_full + trans
+    #####################################################################################################################
+    matches = associate.associate(first_list, third_list, float(args.offset), float(args.max_difference))
+    third_xyz = numpy.matrix(
+        [[float(value) * float(args.scale) for value in third_list[b][0:3]] for a, b in matches]).transpose()
+    rot, trans, trans_error = align(third_xyz, first_xyz)
+    third_stamps = third_list.keys()
+    third_stamps.sort()
+    third_xyz_full = numpy.matrix(
+        [[float(value) * float(args.scale) for value in third_list[b][0:3]] for b in third_stamps]).transpose()
+    third_xyz_full_aligned = rot * third_xyz_full + trans
+    #####################################################################################################################
+    """
+    matches = associate.associate(first_list, fourth_list, float(args.offset), float(args.max_difference))
+    fourth_xyz = numpy.matrix(
+        [[float(value) * float(args.scale) for value in fourth_list[b][0:3]] for a, b in matches]).transpose()
+    rot, trans, trans_error = align(fourth_xyz, first_xyz)
+    fourth_stamps = fourth_list.keys()
+    fourth_stamps.sort()
+    fourth_xyz_full = numpy.matrix(
+        [[float(value) * float(args.scale) for value in fourth_list[b][0:3]] for b in fourth_stamps]).transpose()
+    fourth_xyz_full_aligned = rot * fourth_xyz_full + trans
+    """
+    #####################################################################################################################
+    matches = associate.associate(first_list, fifth_list, float(args.offset), float(args.max_difference))
+    fifth_xyz = numpy.matrix(
+        [[float(value) * float(args.scale) for value in fifth_list[b][0:3]] for a, b in matches]).transpose()
+    rot, trans, trans_error = align(fifth_xyz, first_xyz)
+    fifth_stamps = fifth_list.keys()
+    fifth_stamps.sort()
+    fifth_xyz_full = numpy.matrix(
+        [[float(value) * float(args.scale) for value in fifth_list[b][0:3]] for b in fifth_stamps]).transpose()
+    fifth_xyz_full_aligned = rot * fifth_xyz_full + trans
+    #####################################################################################################################
+    matches = associate.associate(first_list, sixth_list, float(args.offset), float(args.max_difference))
+    sixth_xyz = numpy.matrix(
+        [[float(value) * float(args.scale) for value in sixth_list[b][0:3]] for a, b in matches]).transpose()
+    rot, trans, trans_error = align(sixth_xyz, first_xyz)
+    sixth_stamps = sixth_list.keys()
+    sixth_stamps.sort()
+    sixth_xyz_full = numpy.matrix(
+        [[float(value) * float(args.scale) for value in sixth_list[b][0:3]] for b in sixth_stamps]).transpose()
+    sixth_xyz_full_aligned = rot * sixth_xyz_full + trans
+    #####################################################################################################################
+
+    second_stamps = second_list.keys()
+    second_stamps.sort()
+    second_xyz_full = numpy.matrix(
+        [[float(value) * float(args.scale) for value in second_list[b][0:3]] for b in second_stamps]).transpose()
+    second_xyz_full_aligned = rot * second_xyz_full + trans
+
+    if args.verbose:
+        print "compared_pose_pairs %d pairs" % (len(trans_error))
+
+        print "absolute_translational_error.rmse %f m" % numpy.sqrt(
+            numpy.dot(trans_error, trans_error) / len(trans_error))
+        print "absolute_translational_error.mean %f m" % numpy.mean(trans_error)
+        print "absolute_translational_error.median %f m" % numpy.median(trans_error)
+        print "absolute_translational_error.std %f m" % numpy.std(trans_error)
+        print "absolute_translational_error.min %f m" % numpy.min(trans_error)
+        print "absolute_translational_error.max %f m" % numpy.max(trans_error)
+    else:
+        print "%f" % numpy.sqrt(numpy.dot(trans_error, trans_error) / len(trans_error))
+
+    if args.save_associations:
+        file = open(args.save_associations, "w")
+        file.write("\n".join(
+            ["%f %f %f %f %f %f %f %f" % (a, x1, y1, z1, b, x2, y2, z2) for (a, b), (x1, y1, z1), (x2, y2, z2) in
+             zip(matches, first_xyz.transpose().A, second_xyz_aligned.transpose().A)]))
+        file.close()
+
+    if args.save:
+        file = open(args.save, "w")
+        file.write("\n".join(["%f " % stamp + " ".join(["%f" % d for d in line]) for stamp, line in
+                              zip(second_stamps, second_xyz_full_aligned.transpose().A)]))
+        file.close()
+
+    if args.plot:
+        import matplotlib
+
+        matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
+        import matplotlib.pylab as pylab
+        from matplotlib.patches import Ellipse
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        plot_traj(ax, first_stamps, first_xyz_full.transpose().A, '-', "blue", "ground truth")
+        plot_traj(ax, second_stamps, second_xyz_full_aligned.transpose().A, '-', "green", "cvo")
+        plot_traj(ax, third_stamps, third_xyz_full_aligned.transpose().A, '-', "cyan", "dvo")
+        """plot_traj(ax, fourth_stamps, fourth_xyz_full_aligned.transpose().A, '-', "yellow", "sDvo")"""
+        plot_traj(ax, fifth_stamps, fifth_xyz_full_aligned.transpose().A, '-', "magenta", "open3d_color")
+        plot_traj(ax, sixth_stamps, sixth_xyz_full_aligned.transpose().A, '-', "red", "open3d_hybrid")
+        """
+        label = "difference"
+        for (a, b), (x1, y1, z1), (x2, y2, z2) in zip(matches, first_xyz.transpose().A,
+                                                      second_xyz_aligned.transpose().A):
+            ax.plot([x1, x2], [y1, y2], '-', color="red", label=label)
+            label = ""
+        """
+        ax.legend()
+
+        ax.set_xlabel('x [m]')
+        ax.set_ylabel('y [m]')
+        plt.savefig(args.plot, dpi=90)
