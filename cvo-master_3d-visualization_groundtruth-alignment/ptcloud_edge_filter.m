12c5c6d960ad709f8c9df08b2e16ad062985bfb0
Matt Wilmes <mattbwilmes@gmail.com>
2019-04-23 17:49:49 -0400

    Moved all related files into one folder to prep for merge

---
+++ cvo-master_3d-visualization_groundtruth-alignment/ptcloud_edge_filter.m
@@ -0,0 +1,14 @@
+function clout_out = ptcloud_edge_filter_new(cloud_in)
+% extract edges from an organized RGB-D point cloud
+%   Author: Maani Ghaffari Jadidi
+%   Date:   January 1, 2019
+
+BW = edge(rgb2gray(im2double(cloud_in.Color)),'canny');
+X = reshape(cloud_in.Location,[], 3, 1);
+C = reshape(cloud_in.Color,[], 3, 1);
+% X = X(reshape(BW, [], 1), :);
+% C = C(reshape(BW, [], 1), :);
+C = C(~isnan(X(:,1)),:);
+X = X(~isnan(X(:,1)),:);
+
+clout_out = pointCloud(X, 'Color', C);
